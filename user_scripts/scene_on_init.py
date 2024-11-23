75bea50042978783a1b9aa2abddebd2fbf69da21
JonahCoffelt <coffelt.jonah0918@gmail.com>
2024-11-22 19:30:14 -0600

    Fixed build problems

--- user_scripts/scene_on_init.py
+++ user_scripts/scene_on_init.py
@@ -35,5 +35,5 @@ self.clicked_skeleton = None
 # Rendering scripts
 self.overlay = Overlay(self)
 self.overlay_time = 0
-# self.audio_handler.play_playlist("music")
+self.audio_handler.play_playlist("music")
 # self.cutscene_handler.play_cutscene("intro")
