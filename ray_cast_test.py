ee0e5ffbb964fc5a5ebf0be148609916440cf977
JonahCoffelt <coffelt.jonah0918@gmail.com>
2025-01-28 15:15:50 -0600

    fixed raycast test

--- ray_cast_test.py
+++ ray_cast_test.py
@@ -13,7 +13,7 @@ red = bsk.Material(color=(255, 0, 0))
 blue = bsk.Material(color=(0, 0, 255))
 
 for _ in range(1000):
-    scene.add_node(
+    scene.add(bsk.Node(
         position=[uniform(-100, 100) for _ in range(3)],
         scale=[uniform(0.5, 2) for _ in range(3)],
         rotation=[uniform(0, glm.pi()) for _ in range(3)],
@@ -21,9 +21,7 @@ for _ in range(1000):
         material=blue,
         mesh=sphere,
         static=True
-    )
-
-scene.add(bsk.Node())
+    ))
 
 left_pressed = False
 
@@ -39,10 +37,10 @@ while engine.running:
         
         print(intersection)
         
-        scene.add_node(
+        scene.add(bsk.Node(
             position = intersection,
             material=red,
             scale=(0.1, 0.1, 0.1)
-        )
+        ))
     
     engine.update()
