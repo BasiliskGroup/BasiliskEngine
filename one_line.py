8f37662b61db2bdc1ef3ffd11af07fdee7590a6c
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-02-20 21:19:22 -0600

    Updated syntax for one line

--- one_line.py
+++ one_line.py
@@ -1,14 +1,1 @@
-# import basilisk as bsk
-
-# (hish := __import__('hashlib') if 'hish' not in locals() else hish)
-
-# while (engine := bsk.Engine()) or (engine.update() or engine.running): ...
-
-while any([
-    (bsk := __import__('basilisk') if 'bsk' not in locals() else bsk) is None,
-    (engine := bsk.Engine() if 'engine' not in locals() else engine) is None,
-    (scene := bsk.Scene() if 'scene' not in locals() else scene) is None,
-    setattr(engine, 'scene', scene),
-    engine.update()
-]) or engine.running: ...
-    
+while any([(bsk := __import__('basilisk') if 'bsk' not in locals() else bsk) is None, (engine := bsk.Engine() if 'engine' not in locals() else engine) is None, (scene := bsk.Scene(engine) if 'scene' not in locals() else scene) is None, (node := scene.add(bsk.Node()) if 'node' not in locals() else scene) is None, engine.update(), scene.update()]) or engine.running: ...
