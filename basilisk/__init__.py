f7c6d1db4897547fa3389455462a6ac5f9421d87
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-03-08 22:15:56 -0600

    Exposed a lot of functions

--- basilisk/__init__.py
+++ basilisk/__init__.py
@@ -14,3 +14,13 @@ from .render.post_process import PostProcess
 from .particles.particle_handler import ParticleHandler
 from .render.framebuffer import Framebuffer
 from .audio.sound import Sound
+
+# expose internal algorithms
+from .collisions.narrow.epa import get_epa_from_gjk
+from .collisions.narrow.gjk import collide_gjk
+from .collisions.narrow.graham_scan import graham_scan
+from .collisions.narrow.helper import get_furthest_point, get_support_point
+from .collisions.narrow.line_intersections import line_line_intersect, line_poly_intersect
+from .collisions.narrow.sutherland_hodgman import sutherland_hodgman
+from .generic.collisions import collide_aabb_aabb, collide_aabb_line
+from .generic.meshes import moller_trumbore
