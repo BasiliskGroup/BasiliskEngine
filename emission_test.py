c72d67404da556be262324a737000dad58ef7075
JonahCoffelt <coffelt.jonah0918@gmail.com>
2025-03-15 23:20:15 -0500

    updated test

--- emission_test.py
+++ emission_test.py
@@ -4,7 +4,7 @@ engine = bsk.Engine()
 scene = bsk.Scene(engine)
 # scene.sky = None
 
-emissive_mtl = bsk.Material(emissive_color=(155, 10, 255))
+emissive_mtl = bsk.Material(emissive_color=(2 * 255, 1.5 * 255, 50))
 
 
 cube = bsk.Node(material=emissive_mtl)
