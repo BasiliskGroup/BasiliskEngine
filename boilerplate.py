c61942ae264957e44397168d558b7679c7ed21f5
Jonah Coffelt <coffelt.jonah0918@gmail.com>
2025-03-24 19:07:36 -0500

    fixed boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -2,11 +2,9 @@ import basilisk as bsk
 
 engine = bsk.Engine(title=None)
 scene = bsk.Scene(engine)
-engine.config.bloom_enabled = False
 
 scene.add(bsk.Node())
 
 while engine.running:
-    print('-----------')
     scene.update()
     engine.update(render=False)
