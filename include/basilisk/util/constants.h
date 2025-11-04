2c5f2a9e0ae8c9d9d5fe5adfcd6cb0e896cd9dc4
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-11-04 10:20:08 -0600

    Removed uint from const

--- include/basilisk/util/constants.h
+++ include/basilisk/util/constants.h
@@ -2,19 +2,19 @@
 #define CONSTANTS_H
 
 namespace bsk::internal {
-    inline constexpr uint ROWS = 4;
-    inline constexpr uint MANIFOLD_ROWS = 4;
-    inline constexpr uint JOINT_ROWS = 3;
-    inline constexpr uint SPRING_ROWS = 1;
-    inline constexpr uint NULL_ROWS = 0;
+    inline constexpr unsigned int ROWS = 4;
+    inline constexpr unsigned int MANIFOLD_ROWS = 4;
+    inline constexpr unsigned int JOINT_ROWS = 3;
+    inline constexpr unsigned int SPRING_ROWS = 1;
+    inline constexpr unsigned int NULL_ROWS = 0;
     inline constexpr float PENALTY_MIN = 1000.0f;
     inline constexpr float PENALTY_MAX = 1e9f;
     inline constexpr float STICK_THRESH = 0.02f;
 
     // collision
     inline constexpr float COLLISION_MARGIN = 1e-6f;
-    inline constexpr ushort GJK_ITERATIONS = 15;
-    inline constexpr ushort EPA_ITERATIONS = 15;
+    inline constexpr unsigned short GJK_ITERATIONS = 15;
+    inline constexpr unsigned short EPA_ITERATIONS = 15;
 
     inline constexpr bool SHOW_CONTACTS = true;
 }
