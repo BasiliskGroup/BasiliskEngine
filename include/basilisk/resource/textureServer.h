5287452175f289f47337f9bd55b0acebdd8ac79f
Jonah Coffelt <coffelt.jonah0918@gmail.com>
2026-02-22 12:01:56 -0600

    lowered buckets

--- include/basilisk/resource/textureServer.h
+++ include/basilisk/resource/textureServer.h
@@ -17,7 +17,7 @@ class TextureServer {
         unsigned int getClosestSize(unsigned int x);
 
     public:
-        TextureServer(std::vector<unsigned int> sizeBuckets = {256, 512, 1024, 4048});
+        TextureServer(std::vector<unsigned int> sizeBuckets = {256, 1024, 2048, 2200});
         ~TextureServer();
 
         std::pair<unsigned int, unsigned int> add(Image* image);
